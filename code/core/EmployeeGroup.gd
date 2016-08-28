ded09b3106915728bc0f29a3f3c220588cff0a6c
caryoscelus <caryoscelus@gmx.com>
2016-08-28 11:17:11 +0300

    EmployeeGroup(s)

---
+++ code/core/EmployeeGroup.gd
@@ -0,0 +1,13 @@
+# Contains properties of a group of potential employees
+# - employee type
+# - amount
+# - skills
+# - expected salary range
+extends Node
+
+export(String) var name
+export(String, "worker", "engineer", "scientist") var type
+export(int) var amount
+export(float) var skill
+export(float) var salary_min
+export(float) var salary_great
---
+++ code/core/EmployeeGroups.gd
@@ -0,0 +1,7 @@
+extends Node
+
+func get_group(egroup):
+	return get_child(egroup)
+
+func get_groups():
+	return get_children()

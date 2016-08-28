64058eaf5b3f0ef427969d007d4a8177151b0e7e
caryoscelus <caryoscelus@gmx.com>
2016-08-28 11:16:10 +0300

    Resources & Unit classes

---
+++ code/core/Resources.gd
@@ -0,0 +1,9 @@
+extends Object
+
+var gold = 0
+var rocket = 0
+var nuclear = 0
+var computer = 0
+var rocket_tech = 0
+var nuclear_tech = 0
+var computer_tech = 0
---
+++ code/core/Unit.gd
@@ -0,0 +1,71 @@
+# Unit of employees
+extends Node
+
+var type
+var workers = []
+var engineers = []
+var scientists = []
+var worker_salary
+var engineer_salary
+var scientist_salary
+
+class Bunch:
+	var egroup
+	var amount
+	
+	func turnover(salary):
+		pass
+
+func new_bunch(egroup, amount):
+	var bunch = Bunch.new()
+	bunch.egroup = egroup
+	bunch.amount = amount
+	return bunch
+
+func employ_workers(egroup, amount):
+	workers.append(new_bunch(egroup, amount))
+
+func employ_engineers(egroup, amount):
+	engineers.append(new_bunch(egroup, amount))
+
+func employ_scientists(egroup, amount):
+	scientists.append(new_bunch(egroup, amount))
+
+func worker_amount():
+	return get_amount(workers)
+
+func engineer_amount():
+	return get_amount(engineers)
+
+func scientist_amount():
+	return get_amount(scientists)
+
+func work():
+	"""Do your job, slaves! -- implemented in subclasses"""
+	pass
+
+func turnover():
+	"""Slaves might like or might not like to be slaves"""
+	for bunch in workers:
+		bunch.turnover(worker_salary)
+	for bunch in engineers:
+		bunch.turnover(engineer_salary)
+	for bunch in scientists:
+		bunch.turnover(scientist_salary)
+
+func pass_time():
+	work()
+	turnover()
+
+func get_amount(employees):
+	total = 0
+	for bunch in employees:
+		total += bunch.amount
+	return total
+
+func get_power(employees):
+	var total = 0
+	for bunch in employees:
+		var group = EmployeeGroups.get(bunch.egroup)
+		total += bunch.amount * group.skill
+	return total

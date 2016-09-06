46128f9cc6e27531cd5889381f925a4d6f487eb9
caryoscelus <caryoscelus@gmx.com>
2016-09-06 11:27:11 +0300

    new WorldMap; Europa zone for testing

--- code/world/WorldMap.gd
+++ code/world/WorldMap.gd
@@ -1,7 +1,42 @@
-# world map
+##
+##  Copyright (C) 2016 caryoscelus
+##
+##  This file is part of second-chance game
+##  https://github.com/caryoscelus/second-chance
+##  
+##  This program is free software: you can redistribute it and/or modify
+##  it under the terms of the GNU General Public License as published by
+##  the Free Software Foundation, either version 3 of the License, or
+##  (at your option) any later version.
+##  
+##  This program is distributed in the hope that it will be useful,
+##  but WITHOUT ANY WARRANTY; without even the implied warranty of
+##  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+##  GNU General Public License for more details.
+##  
+##  You should have received a copy of the GNU General Public License
+##  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+##
+
+## World Map
+
 extends TextureFrame
 
-onready var stats = get_node("stats")
+onready var zones = get_node("zones")
 
-func _on_show_stats_button_pressed():
-	stats.popup()
+func _ready():
+	var world = SCWorld
+	# for test
+	world.zones.append(load("res://scenes/data/zones/Europa.tscn").instance())
+	for zone in world.zones.get_all():
+		var zone_node = TextureButton.new()
+		var gfx_path = "res://gfx/world/zone-%s"%zone.get_name()
+		var mask = load(gfx_path+".pbm")
+		if mask != null:
+			var texture = load(gfx_path+".png")
+			zone_node.set_click_mask(mask)
+			zone_node.set_hover_texture(texture)
+		else:
+			# TODO: add some generic button
+			pass
+		zones.add_child(zone_node)

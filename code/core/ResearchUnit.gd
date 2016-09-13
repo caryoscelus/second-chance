250939a5df5931e09cae362f99dcb51a1d0c519d
caryoscelus <caryoscelus@gmx.com>
2016-09-13 13:53:39 +0300

    empty ResearchUnit

--- code/core/ResearchUnit.gd
+++ code/core/ResearchUnit.gd
@@ -1,8 +1,29 @@
+##
+##  Copyright (C) 2016 caryoscelus
+##
+##  This file is part of second-chance game
+##  https://github.com/caryoscelus/second-chance
+##  
+##  This program is free software: you can redistribute it and/or modify
+##  it under the terms of the GNU General Public License as published by
+##  the Free Software Foundation, either version 3 of the License, or
+##  (at your option) any later version.
+##  
+##  This program is distributed in the hope that it will be useful,
+##  but WITHOUT ANY WARRANTY; without even the implied warranty of
+##  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+##  GNU General Public License for more details.
+##  
+##  You should have received a copy of the GNU General Public License
+##  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+##
+
+## A research unit. Only science can save the world now!
+
 extends "Unit.gd"
 
 func _init():
-	type = "research"
+	task = "research"
 
-func work():
-	var resources = Resources.get_empty()
-	return resources
+func process(dt):
+	pass
